78d4b227d9515af7b658b9508f1b3294c6ff037c
Subhrannill <subhranildutta2022@vitbhopal.ac.in>
2024-05-01 00:27:33 +0530

    This is a Supabase Auth Project

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,13 +7,12 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  const origin = requestUrl.origin;
 
   if (code) {
     const supabase = createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  // URL to redirect to after sign up process completes
-  return NextResponse.redirect(`${origin}/protected`);
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin);
 }
